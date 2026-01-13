f188ab8785eac4ab62733fec83cb95ccf9ec2b04
William Reyes Valencia <williamreyesvalencia04@gmail.com>
2026-01-13 21:59:32 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4318642261-64aa8",
+  "appId": "1:49623550774:web:a0a99d29a2367d1a0c8197",
+  "apiKey": "AIzaSyBHxbtvc_9HhvBgYEq7_tPnPjETHIzrxL8",
+  "authDomain": "studio-4318642261-64aa8.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "49623550774"
+};
